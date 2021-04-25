d5eb4ca2baf0bda5cc0774c0f996ee841884f3d8
jiuyuedongyang <lin909958300@yeah.net>
2021-04-25 17:42:41 +0800

    初始化

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
 }
 func main() {
-	fmt.Fprint(1)
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
 }
