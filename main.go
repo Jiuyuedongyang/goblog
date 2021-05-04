8dcdfeef73083467149afa1fdfa32cc00276cdcd
jiuyuedongyang <lin909958300@yeah.net>
2021-05-04 14:55:44 +0800

    重构中间件2

--- main.go
+++ main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"goblog/app/http/middlewares"
-
-	_ "github.com/go-sql-driver/mysql"
-
 	"goblog/bootstrap"
 	"net/http"
 )
 
 func main() {
-
 	bootstrap.SetupDB()
 	router := bootstrap.SetupRoute()
-
 	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
